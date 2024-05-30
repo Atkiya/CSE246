55f91f7b7aa55a1896434bfc59418a5f66dd3220
Atkiya <115377605+Atkiya@users.noreply.github.com>
2024-05-30 18:04:18 +0600

    Update maxflow.cpp

--- maxflow.cpp
+++ maxflow.cpp
@@ -1,81 +1,88 @@
-#include<bits/stdc++.h>
-#define ll long long
+#include <bits/stdc++.h>
 using namespace std;
-const int N=5005;
-const int M=30005*2;
-const ll INF=100000000000LL;
-int u[M],v[M],first[N],Next[M],e;
-ll cap[M] = {0},flow[M];
-int q[N],dist[N],work[N],source,sink;
-int dirx[]={0,1,0,-1};
-int diry[]={1,0,-1,0};
-void init()
-{
-    memset(first,-1,sizeof(first));
-    e=0;
-}
-void insert(int f,int t,ll val)
-{
-    u[e]=f; v[e]=t; cap[e]=max(cap[e],val); flow[e]=0;
-    Next[e]=first[u[e]]; first[u[e]]=e++;
-    u[e]=t; v[e]=f; cap[e]=max(cap[e],val); flow[e]=0;
-    Next[e]=first[u[e]]; first[u[e]]=e++;
-}
-bool bfs()
-{
-    memset(dist,-1,sizeof(dist));
-    dist[source]=0;
-    int l=0,r=0;
-    q[r++]=source;
-    while(l<r){
-        int x=q[l++];
-        for(int i=first[x];i+1;i=Next[i]){
-            if(flow[i]<cap[i]&&dist[v[i]]==-1){
-                dist[v[i]]=dist[x]+1;
-                q[r++]=v[i];
+typedef long long ll;
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+#define MAX 100005
+#define all(v) v.begin(), v.end()
+#define SPEED ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const int MOD = 1e9+7;
+const int MODD = 998244353;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+// template <typename num_t>
+// using ordered_set = tree<num_t, null_type, less<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+
+
+ll fordFulkerson(int n,vector<vector<ll>>&g){
+    ll mx=0;
+    while(1){
+        ll flow=LLONG_MAX;
+        int vis[n+1]{},par[n+1]{};
+        queue<int>q;
+        q.push(1);
+        vis[1]=1;
+        par[1]=-1;
+
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(int i=1;i<=n;i++){
+                if(vis[i]==0 and g[u][i]>0){
+                    q.push(i);
+                    par[i]=u;
+                    vis[i]=1;
+                }
             }
         }
-    }
-    return dist[sink]!=-1;
-}
-int dfs(int x,ll val)
-{
-    if(x==sink) return val;
-    for(int &i=work[x];i+1;i=Next[i]){
-        int y=v[i]; ll tmp;
-        if(flow[i]<cap[i]&&dist[y]==dist[x]+1&&(tmp=dfs(y,min(val,cap[i]-flow[i])))>0){
-            flow[i]+=tmp;
-            flow[i^1]-=tmp;
-            return tmp;
+        if(vis[n]==0)break;
+        for(int v=n;v!=1;v=par[v]){
+            int u=par[v];
+            flow=min(flow,g[u][v]);
         }
+    
+        if(flow==LLONG_MAX)break;
+        for(int v=n;v!=1;v=par[v]){
+            int u=par[v];
+            g[u][v]-=flow;
+            g[v][u]+=flow;
+        }
+        mx+=flow;
+    }
+    return mx;
+}
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<ll>>g(n+1,vector<ll>(n+1));
+    for(int i=0;i<m;i++){
+        ll u,v,w;
+        cin>>u>>v>>w;
+        if(u==v)continue;
+        g[u][v]+=w;
+    }
+    cout<<fordFulkerson(n,g)<<'\n';
+}
+ 
+ 
+int main(){
+    SPEED;
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+ 
+    int t = 1;
+    cin>>t;
+    int T = 0;
+    while(t--) {
+        T++;
+        cout<<"Case "<<T<<": ";
+        solve();
     }
     return 0;
 }
-ll dinic()
-{
-    ll ans=0;
-    while(bfs()){
-        for(int i=0;i<N;i++) work[i]=first[i];
-        while(1){
-            ll tmp=dfs(source,INF);
-            if(tmp==0) break;
-            ans+=tmp;
-        }
-    }
-    return ans;
-}
-int main()
-{
-    int n,m,a,b,c;
-    scanf("%d%d",&n,&m);
-    init();
-    source = 1, sink = n;
-    while(m--){
-        scanf("%d%d%d",&a,&b,&c);
-        if(a!=b){
-            insert(a,b,c);
-        }
-    }
-    printf("%lld\n",dinic());
-    return 0;
-}
+ 
